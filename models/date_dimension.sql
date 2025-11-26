79134c99bac8dd95e732a13cb95b99f40694208d
suryasurendar16@gmail.com <suryasurendar16@gmail.com>
2025-11-26 12:01:20 +0000

    last update for prod movement

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='table') }}
 with result as (
     select 
-    to_timestamp(STARTTIME,'MM/DD/YYYY HH:MI') STARTTIME,
-    to_date(to_timestamp(STARTTIME,'MM/DD/YYYY HH:MI')) DATE_STARTDATE,
-    HOUR(to_timestamp(STARTTIME,'MM/DD/YYYY HH:MI')) HOUR_STARTDATE,
-    DAYOFWEEK(to_timestamp(STARTTIME,'MM/DD/YYYY HH:MI')) DAYOFWEEK_STARTDATE,
-    DAYNAME(to_timestamp(STARTTIME,'MM/DD/YYYY HH:MI')) DAYNAME_STARTDATE,
+    to_timestamp(STARTTIME) STARTTIME,
+    date(to_timestamp(STARTTIME)) DATE_STARTDATE,
+    HOUR(to_timestamp(STARTTIME)) HOUR_STARTDATE,
+    DAYOFWEEK(to_timestamp(STARTTIME)) DAYOFWEEK_STARTDATE,
+    DAYNAME(to_timestamp(STARTTIME)) DAYNAME_STARTDATE,
     {{get_day_type('STARTTIME')}}Day_Type,
     {{get_season('STARTTIME')}} STATION_OF_YEAR
     
